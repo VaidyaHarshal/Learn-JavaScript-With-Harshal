b9d538c9572209b9e58973b2052e40031b349bf7
Harshal Vaidya <harshal.vaidya300@gmail.com>
2024-12-16 15:51:04 -0500

    operand and boolean

---
+++ Day4.js
@@ -0,0 +1,14 @@
+// 4. What's the output?
+
++true;
+!"Harshal";
+
+// A: 1 and false
+// B: false and NaN
+// C: false and false
+
+// Answer: A
+// The unary plus tries to convert an operand to a number. true is 1, and false is 0.
+
+// The string 'Lydia' is a truthy value.
+// What we're actually asking, is "Is this truthy value falsy?". This returns false.
