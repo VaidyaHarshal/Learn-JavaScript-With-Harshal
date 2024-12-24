9856e0cf34bb2e6c4d14a7c56f498a218290a767
Harshal Vaidya <harshal.vaidya300@gmail.com>
2024-12-23 21:10:12 -0500

    change in data

--- Day11.js
+++ Day11.js
@@ -5,7 +5,7 @@ function Person(firstName, lastName) {
   this.lastName = lastName;
 }
 
-const member = new Person("Lydia", "Hallie");
+const member = new Person("Vaidya", "Harshal");
 Person.getFullName = function () {
   return `${this.firstName} ${this.lastName}`;
 };
