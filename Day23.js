4d77451d0067adc6d571aba254a58283b2a96645
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-01-04 15:51:00 -0500

    var variables can be declared multiple times

--- Day23.js
+++ Day23.js
@@ -9,3 +9,8 @@ console.log(num);
 // B: 10
 // C: SyntaxError
 // D: ReferenceError
+
+// Answer: B
+// With the var keyword, you can declare multiple variables with the same name. The variable will then hold the latest value.
+
+// You cannot do this with let or const since they're block-scoped and therefore can't be redeclared.
