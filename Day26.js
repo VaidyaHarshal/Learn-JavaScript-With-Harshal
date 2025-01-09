df6f5e5d42deb6fcc27dace8ebcc11a6761b40c4
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-01-08 20:36:14 -0500

    global execution context creates two things

---
+++ Day26.js
@@ -0,0 +1,7 @@
+// 26. The JavaScript global execution context creates two things for you: the global object, and the "this" keyword.
+// A: true
+// B: false
+// C: it depends
+
+// Answer: A
+// The base execution context is the global execution context: it's what's accessible everywhere in your code.
