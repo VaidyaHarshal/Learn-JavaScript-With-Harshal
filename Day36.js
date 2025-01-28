09365e15e3d2f4a2a9f56ec39bf726f31df2d520
Harshal Vaidya <harshal.vaidya300@gmail.com>
2025-01-27 20:57:32 -0500

    typeof "number"

---
+++ Day36.js
@@ -0,0 +1,11 @@
+// 36. What's the output?
+
+console.log(typeof typeof 1);
+
+// A: "number"
+// B: "string"
+// C: "object"
+// D: "undefined"
+
+// Answer: B
+// typeof 1 returns "number". typeof "number" returns "string"
